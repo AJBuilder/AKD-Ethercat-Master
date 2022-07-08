c07089671bf4f7196a9dadd70fb5a4ad6d77a68f
root <adam.aj.robertson@gmail.com>
2022-07-08 17:20:03 +0000

    Reorganized

--- build/CMakeFiles/DS402.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/DS402.dir/src/AKDEcatController.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/DS402.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
